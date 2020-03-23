3fe1218002daa6d4372e0c75ce8b2810b0044193
Brendan Williams <45268630+bwilliams96@users.noreply.github.com>
2020-03-23 13:59:42 +0000

    Create main.m

---
+++ main.m
@@ -0,0 +1,37 @@
+function main()
+
+%% Refresh Matlab
+
+sca;
+close all;
+clearvars;
+RandStream.setGlobalStream(RandStream('mt19937ar','seed',sum(100*clock)));
+
+%% Task parameters --------------------------------------------------------
+
+
+%% ------------------------------------------------------------------------
+
+%% Participant info input
+subjectID = input('Enter participant number: ', 's');
+
+%ListenChar(2); Commented out for testing
+%% Psychtoolbox setup
+
+PsychDefaultSetup(2);
+KbName('UnifyKeyNames');
+screens = Screen('Screens');
+screenNumber = max(screens);
+white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
+grey = white / 1.5;
+inc = white - grey;
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey, [0 0 200 200]);
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+ifi = Screen('GetFlipInterval', window);
+[xCenter, yCenter] = RectCenter(windowRect);
+Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
+
+%% Output file setup
+
+end
