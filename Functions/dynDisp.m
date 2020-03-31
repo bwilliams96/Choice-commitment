eb88f75baad9cc6f4d07212eb1c965b2fec4b64d
Brendan Williams <45268630+bwilliams96@users.noreply.github.com>
2020-03-31 14:43:46 +0100

    Fix error selecting stimuli for first trial

--- Functions/dynDisp.m
+++ Functions/dynDisp.m
@@ -17,18 +17,29 @@ matrix = The updated input matrix
 
 %}
 
-% Find the proportion of trials that each stimuli has been chosen, minus
-% the proportion of chosen trials of the stimulus with the largest
-% proportion, squared
-prob = matrix(2,:)/sum(matrix(2,:));
-maximum = max(prob);
-prob = (prob-maximum).^2;
+disp1 = 0;
+disp2 = 0;
 
-% Find the two least displayed images (these will have the largest and
-% second largest numbers)
-[~, disp1] = max(prob);
-prob(1, disp1) = 0;
-[~, disp2] = max(prob);
+% Randomly chose stimuli for the first trial
+if sum(matrix(2,:)) == 0
+    while disp1 == disp2
+        disp1 = randi(length(matrix));
+        disp2 = randi(length(matrix));
+    end
+else
+    % Find the proportion of trials that each stimuli has been chosen, minus
+    % the proportion of chosen trials of the stimulus with the largest
+    % proportion, squared
+    prob = matrix(2,:)/sum(matrix(2,:));
+    maximum = max(prob);
+    prob = (prob-maximum).^2;
+    
+    % Find the two least displayed images (these will have the largest and 
+    % second largest numbers)
+    [~, disp1] = max(prob);
+    prob(1, disp1) = 0;
+    [~, disp2] = max(prob);
+end
 
 % Update count for the displayed images in the matrix
 matrix(1, disp1) = matrix(1, disp1) + 1;
