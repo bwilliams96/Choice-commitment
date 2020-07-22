82cca283ce9fbba811462c5777a451fb478e9951
Brendan Williams <45268630+bwilliams96@users.noreply.github.com>
2020-07-22 17:22:26 +0100

    Add function to create outcome arrays

--- Javascript/functions/functions.js
+++ Javascript/functions/functions.js
@@ -20,7 +20,7 @@ function dynDisp(object) {
     chosen an equal number of times
     */
     var nchoice = [object[2][0][1], object[2][1][1], object[2][2][1], object[2][3][1]];
-    range = Math.max(...nchoice) - Math.min(...nchoice);
+    var range = Math.max(...nchoice) - Math.min(...nchoice);
     if (range == 0) {
         while (object[0] == object[1]) {
             object[0] = Math.floor(Math.random()*4);
@@ -47,4 +47,68 @@ function dynDisp(object) {
     }
 
     return object
-}
+};
+
+function makeMatrix(l1, l2, prob, trueLength) {
+    /*
+
+    Created by: Brendan Williams
+
+    PLEASE NOTE!!!
+    OBSERVED PROBABILITIES WILL NOT RELIABLY MATCH WITH EXPECTED PROBABILITIES
+    IF prob*trueLength OR prob*(1-trueLength) ARE NOT INTEGERS DUE TO ROUNDING.
+
+    Function requires 4 inputs:
+    l1 = number of trials before choice commitment question
+    l2 = number of trials after choice commitment
+    prob = probability of winning for each option after choice commitment
+    trueLength = number of trials over which expected probabilities are observed.
+
+    The function has 1 output:
+    output[0] = binary matrix of outcomes before choice commitment with 
+    (l1,length(prob)) dimensions. 0 represents a loss and 1 represents a win.
+
+    output[1] = binary matrix of outcomes after choice commitment with 
+    (l1,length(prob)) dimensions. 0 represents a loss and 1 represents a win.
+
+    */
+
+   function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+   var l1tL = Math.ceil(l1 / trueLength);
+   var l2tL = Math.ceil(l2 / trueLength);
+
+   var output = [];
+   output[0] = [[], [], [], []];
+   output[1] = [[], [], [], []];
+
+   for (let i = 0; i < l1tL; i++) {
+       var temp = [[], [], [], []];
+       for (let ii = 0; ii < prob.length; ii++) {
+           var z = Array(Math.ceil(trueLength / 2)).fill(0);
+           var o = Array(Math.ceil(trueLength / 2)).fill(1);
+           var zo = z.concat(o);
+           shuffle(zo);
+           output[0][ii] = output[0][ii].concat(zo);
+        }
+   }
+
+   for (let i = 0; i < l2tL; i++) {
+    var temp = [[], [], [], []];
+    for (let ii = 0; ii < prob.length; ii++) {
+        var probability = prob[ii] / 100;
+        var z = Array(Math.floor(trueLength * (1-probability))).fill(0);
+        var o = Array(Math.ceil(trueLength * probability)).fill(1);
+        var zo = z.concat(o);
+        shuffle(zo);
+        output[1][ii] = output[1][ii].concat(zo);
+        }
+    }
+
+    return output
+};
